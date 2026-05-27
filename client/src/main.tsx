0e861b8fb5445e451ef06bb58aa50f9be0b7ce9a
Ronan Cotillard <ronan.cotillard@gmail.com>
2026-05-27 23:10:24 +0200

    ci: force redeploy via watched main.tsx modification

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Force redeploy - 2026-05-27
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
